aaf7761a210f2ee899f07ca5de21367375101d5c
Suresh Tipirneni <tipirneni@yahoo.com>
2022-03-03 18:08:39 -0600

    Create convertslashntonewline.pl

---
+++ convertslashntonewline.pl
@@ -0,0 +1,13 @@
+#Suresh Tipirneni 
+#convert error \n to newline in a file
+my $read;
+open(FILE, "$ARGV[0]") || die "Can't open $ARGV[0]!";
+open(my $fh, '>', "$ARGV[0].new") or die "Could not open file '$ARGV[0]' $!";
+my $pc1='';
+while ($read = read FILE, $char, 1) {
+    if ($pc1 eq '\\' && $char eq 'n') {  print $fh "\n"; }
+    else { print $fh "$pc1"; }
+    $pc1=$char;
+}
+print $fh $pc1;
+close $fh;
