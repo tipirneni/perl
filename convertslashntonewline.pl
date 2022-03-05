c1b214ce4086d62cffbfd0cea1798491cb6b7418
Suresh Tipirneni <tipirneni@yahoo.com>
2022-03-05 10:05:02 -0600

    Update convertslashntonewline.pl

--- convertslashntonewline.pl
+++ convertslashntonewline.pl
@@ -5,7 +5,7 @@ open(FILE, "$ARGV[0]") || die "Can't open $ARGV[0]!";
 open(my $fh, '>', "$ARGV[0].new") or die "Could not open file '$ARGV[0]' $!";
 my $pc1='';
 while ($read = read FILE, $char, 1) {
-    if ($pc1 eq '\\' && $char eq 'n') {  print $fh "\n"; }
+    if ($pc1 eq '\\' && $char eq 'n') {  print $fh "\n"; $char='';}
     else { print $fh "$pc1"; }
     $pc1=$char;
 }
